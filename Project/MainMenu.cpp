b7258347e10bdbd5b5f6f5b75bc544bcf5cf6f4f
TwEight3 <oliver.askew@student.adelaide.edu.au>
2022-09-18 16:52:23 +0930

    Initial Commit

---
+++ Project/MainMenu.cpp
@@ -0,0 +1,9 @@
+//
+//  MainMenu.cpp
+//  Project
+//
+//  Created by Oliver Askew on 18/9/2022.
+//  Copyright © 2022 Oliver Askew. All rights reserved.
+//
+
+#include "MainMenu.hpp"
---
+++ Project/MainMenu.hpp
@@ -0,0 +1,14 @@
+//
+//  MainMenu.hpp
+//  Project
+//
+//  Created by Oliver Askew on 18/9/2022.
+//  Copyright © 2022 Oliver Askew. All rights reserved.
+//
+
+#ifndef MainMenu_hpp
+#define MainMenu_hpp
+
+#include <stdio.h>
+
+#endif /* MainMenu_hpp */
