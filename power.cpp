2ab9d9c303aa165f656cadbadfccb3d92b47cf3d
Erfan5555 <109932909+Erfan5555@users.noreply.github.com>
2022-09-28 11:15:21 +0930

    s

---
+++ power.cpp
@@ -0,0 +1,256 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+// abstract class since the method rebel and kill king is not implemented.
+// No object of this class is intendeded to be instantiated.
+
+class show_all_variables { // intended for programmers only
+    public:
+    virtual void show_vars()=0;
+    
+}
+
+
+class power:public show_all_variables{
+protected :
+int King_Popularity;
+int Overall_value;
+bool game_switch; // This ends the king's life
+
+public:
+
+power (int popularity, int satsifactionValue): King_Popularity(popularity),
+Overall_value(satsifactionValue){game_switch=true;} // intlisaed the constructor of 
+//power
+
+power(): power(100,100){game_switch=true;} // intliases the constructor if no values are inputed
+
+virtual void Rebel ()=0;
+
+virtual int set_overall_value()=0; //Overall value is the number seen 
+//on the bottom of each sign of a power. it is calculated from different variables
+// in each power. 
+// set_overall_value will be put in after each time a card has been drawen and the user
+// makes a decsion
+
+void show_vars(){
+    cout<<"kings's popularity:"<<King_Popularity<<endl;
+    cout<<"overall value is: "<<Overall_value<<endl;
+}
+
+void kill_king(){ // INSERT THE GAME END FUNCTION
+    game_switch=false;
+    cout<<"game ended"<<endl;
+}
+
+
+};
+
+
+class people :public power{
+protected :
+int Employment;
+int Entertainmnet;
+int Food_avaliable;
+
+public :
+people(int Employment_new_Value, int Entertainmnet_new_Value, 
+int Food_avaliable_new_Value): power(50,50), Employment(Employment_new_Value),Entertainmnet(Entertainmnet_new_Value)
+,Food_avaliable(Food_avaliable_new_Value){} // intilisasing the constructor
+
+people(): people(25,25,25){}
+
+void Rebel ( ){ // polymorphism here
+    Employment=Employment-70;
+    King_Popularity= King_Popularity-70;
+
+}
+
+int set_overall_value(){ // 
+   int value= Employment+ Entertainmnet+Food_avaliable+King_Popularity;
+    Overall_value= value;
+    if (value<0){
+        value=0;
+        game_switch=false; // ends the king's life and start a new game.
+    }
+    if (value>100){
+        value =100;
+        game_switch=false; // ends the king's life and start a new game.
+    }
+    return value;
+}
+
+
+
+void change_employment (int Employment ){
+this->Employment = Employment;
+}
+
+void change_entertainment (int Entertainment ){
+   this-> Entertainmnet= Entertainment;
+}
+
+void change_food_avaliable (int Food_avaliable){
+    this-> Food_avaliable = Food_avaliable;
+}
+
+
+void show_vars(){
+    cout<<"employment is:"<<Employment<<endl;
+    cout<<"avaiable food is:"<<Food_avaliable<<endl;
+    cout<<"Entertainment value is:"<<Entertainmnet<<endl;
+    cout<<"overall value is: "<<Overall_value<<endl;
+}
+
+
+};
+
+class church : public power {
+protected:
+int number_of_churches;
+int wealth_of_pop;
+ 
+public :
+// intlisases the church constructors 
+church (int number, int wealth): power(50,50),number_of_churches(number), wealth_of_pop(wealth){}
+
+church (): church(100,100){}
+
+void change_number_of_churches (int new_number){
+this->number_of_churches= new_number;
+}
+
+void Rebel ( ){ // polymorphism here
+    
+    this-> number_of_churches = (number_of_churches-50);
+    
+}
+
+void change_wealth_of_pop(int new_wealth){
+    wealth_of_pop=new_wealth;
+
+}
+
+    int set_overall_value(){ // 
+
+    int value= number_of_churches+wealth_of_pop;
+    Overall_value= value;
+
+    if (value<0){
+        value=0;
+        game_switch=false;
+    }
+    if (value>100){
+        value =100;
+        game_switch=true;
+    }
+    return value;
+}
+ int show_overall_value(){
+        cout<<"the overall value is:"<< Overall_value <<endl;
+        return Overall_value;
+    }
+
+    void show_vars(){
+    cout<<"church number value is:"<<number_of_churches<<endl;
+    cout<<"wealth of the pop is:"<<wealth_of_pop<<endl;
+   
+    cout<<"overall value is: "<<Overall_value<<endl;
+}
+
+  
+
+};
+
+
+
+class Army: public power {
+    protected:
+    int numb_of_troops;
+    int quality_of_weapons;
+    int food_avaliblity;
+
+
+    public:
+    Army(int numb_of_troops, int quality_of_weapons,int food_avaiable):power(25,50), numb_of_troops(numb_of_troops),
+    quality_of_weapons(quality_of_weapons),food_avaliblity(food_avaiable){}
+
+    Army (): Army(25,25,25){};
+
+    void Rebel(){
+        numb_of_troops=numb_of_troops-50;
+    }
+
+   int set_overall_value(){
+        int value=numb_of_troops+quality_of_weapons+food_avaliblity;
+       Overall_value= value;
+
+        if (value<0){
+        value=0;
+        game_switch=false;
+    }
+    if (value>100){
+        value =100;
+        game_switch=true;
+    }
+    return value;
+    }
+
+    int show_overall_value(){
+        cout<<"the overall value is:"<< Overall_value <<endl;
+               return Overall_value;
+    }
+    void show_vars(){
+    cout<<"number of troops is:"<<numb_of_troops<<endl;
+    cout<<" Quality of weapons is:"<<quality_of_weapons<<endl;
+     cout<<" Avaiable food is:"<<food_avaliblity<<endl;
+    
+    cout<<"overall value is: "<<Overall_value<<endl;
+}
+
+  
+  
+
+};
+
+class wealth:public power{
+    protected :
+    int wealth_of_king;
+
+    public:
+    wealth(int wealth): power(50,50), wealth_of_king(wealth){}; // intilisaing the values
+    wealth(): wealth(50){}; // defult constructor with no input values
+    
+    void set_weatlh(int new_wealth){
+        wealth_of_king=new_wealth;
+    }
+
+  void show_vars(){
+    cout<<"wealth of the king is:"<<wealth_of_king<<endl;
+    
+    
+    cout<<"overall value is: "<<Overall_value<<endl;
+}
+    
+};
+
+
+
+
+
+int main (){ // This is just for testing of classes
+
+people k(25,25,25);
+
+church f(90,0);
+Army s(4,55,55); // TEST THE SHOW_OVERALL_VALUES
+s.set_overall_value();
+k.kill_king();
+s.show_overall_value();
+cout<<"This is the value for church:";
+f.set_overall_value();
+f.show_overall_value();
+
+}
